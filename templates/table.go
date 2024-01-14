716931a3d9dbff9a9024929f315053e7abb023f9
xen <selegakamil@gmail.com>
2024-01-14 16:29:05 +0100

    Update index.html based on dashboard admin template

--- templates/table.go
+++ templates/table.go
@@ -3,20 +3,11 @@ package templates
 import "html/template"
 
 var Table = template.Must(template.New("table").Parse(`
-	<table>
-		<tr>
-			<th>Id</th>
-			<th>CreatedAt</th>
-			<th>Type</th>
-			<th>Value</th>
-		</tr>
-		{{range $r := .Records}}
-		<tr>
-			<td>{{$r.Id}}</td>
-			<td>{{$r.CreatedAt}}</td>
-			<td>{{$r.Type}}</td>
-			<td>{{$r.Value}}</td>
-		</tr>
-		{{end}}
-	</table>
+	{{range $r := .Records}}
+	<tr>
+		<td>{{$r.CreatedAt}}</td>
+		<td>{{$r.Type}}</td>
+		<td>{{$r.Value}}</td>
+	</tr>
+	{{end}}
 `))
